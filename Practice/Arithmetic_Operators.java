c2f3b54372eea3730c0ae1db489d2f816f382d2d
Sankho1769 <banerjeeshubham00@gmail.com>
2026-07-31 00:03:46 +0530

    Learn some fundamentals

--- Practice/Arithmetic_Operators.java
+++ Practice/Arithmetic_Operators.java
@@ -1,5 +1,28 @@
 package Practice;
 
 public class Arithmetic_Operators {
-    
+    public static void main(String[] args) {
+        
+        int a = 20;
+        int b = 10;
+
+       int Sum = a + b;
+       int Difference = a - b;
+       int Product = a * b ;
+       int Quotient = a / b;
+       int Remainder = a % b;
+
+        System.out.println("Sum : " + Sum);
+        System.out.println("Difference : " + Difference);
+        System.out.println("Product : " + Product);
+        System.out.println("Quotient: " + Quotient);
+        System.out.println("Remainder: " + Remainder);
+
+        // 10 + 5 * 2 - 8 / 4
+        
+        int result = (10 + 5 * 2 - 8 / 4);
+
+        System.out.println("This is the resut : " + result);
+
+ }
 }
---
+++ Practice/IncrementDecrement.java
@@ -0,0 +1,23 @@
+package Practice;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        System.out.println(x);
+        System.out.println("After Increment:" + x++);
+        System.out.println("After Decrement:" + x--);
+
+        int a = 10;
+
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+        System.out.println(--a);
+ 
+
+    }
+}
+
+
