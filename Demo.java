fe8c23534f5d10b3611321f8b327b18f6a6acfdc
Sankho1769 <banerjeeshubham00@gmail.com>
2026-08-02 00:31:58 +0530

    Day 3 of learning

--- Demo.java
+++ Demo.java
@@ -1,26 +1,53 @@
-class calculator {
+// class Calculator {
 
-    int a;
-    int b;
+//     int a;
+//     int b;
     
-    public int add() {
-        System.out.println("in add");
-        return -0;
+//     public int add(int n1, int n2) {
+//         int r = n1 + n2;
+//         return r;
+//     }
+// }
+
+// public class Demo {
+//     public static void main(String[] args) {
+        
+//         int num1 = 7;
+//         int num2 = 9;
+
+//         Calculator calc = new Calculator();
+
+//         int result = calc.add(num1, num2);
+
+//         // int result = num1 + num2;
+
+//         System.out.print("The sum is : " + result);
+//     }
+// }
+
+class Computer {
+   public void playMusic() {
+        System.out.println("Music Playing..");
+    }
+
+    public String getMeAPen(int cost){
+
+        if (cost >= 10)
+            return "pen";
+        else
+            return "Nothing";
     }
 }
 
 public class Demo {
     public static void main(String[] args) {
         
-        int num1 = 7;
-        int num2 = 9;
 
-        calculator calc = new calculator();
+        Computer obj = new Computer();
+        obj.playMusic();
+        obj.getMeAPen(10);
+        String str = obj.getMeAPen(2);
+        System.out.println(str);
 
-        calc.add();
-
-        // int result = num1 + num2;
-
-        // System.out.print("The sum is : " + result);
     }
 }
--- Practice/ifelse.java
+++ Practice/ifelse.java
@@ -43,14 +43,87 @@ public class ifelse {
         //     System.out.println("Equal");
         // }
 
-        int year = 2024;
+        // int year = 2024;
 
-        if (( year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ) {
-            System.out.println("This is a leap year " + year);
-        } else {
-            System.out.println("This is not a leap year " + year);
-        }
+        // if (( year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ) {
+        //     System.out.println("This is a leap year " + year);
+        // } else {
+        //     System.out.println("This is not a leap year " + year);
+        // }
  
+        
+        // int a = 25;
+        // int b = 40;
+        // int c = 188;
+
+        // if (a > b && a > c) {
+        //     System.out.println("A is larger" + a);
+        // } else if (b > a && b > c) {
+        //     System.out.println("B is Larger" + b);
+        // } else if (c > a && c > b) {
+        //     System.out.println("C is Larger" + c);
+        // } else {
+        //     System.out.println("Equal");
+        // }
+
+
+        //   char ch = 'e';
+
+        // if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+        //         || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+
+        //     System.out.println(ch + " is a vowel");
+
+        // } else {
+
+        //     System.out.println(ch + " is a consonant");
+
+        // }
+
+        // int num = 35;
+
+        // if (num % 5 == 0) {
+        //     System.out.println(num + " is divisable by 5");
+        // } else {
+        //     System.out.println(num + " is not divisable by 5");
+        // }
+
+        //  int num = 35;
+
+        // if (num % 5 == 0 || num % 3 == 0) {
+        //     System.out.println(num + " is divisible by 5 & 3");
+        // } else {
+        //     System.out.println(num + " is not divisible by 5 & 3");
+        // }
+
+        // int age = 18;
+        // boolean hasVoterId = false;
+
+        // if (age >= 18) {
+        //     if (hasVoterId == true) {
+        //         System.out.println("You can vote"); 
+        //     } else {
+        //         System.out.println("Apply for voter card");
+        //     }
+        // }
+        //     else {
+        //         System.out.println("Not eligebile for vote");
+        //     }
+
+
+        double salary = 60000;
+        int experience = 3;
+
+                if (salary >= 60000) {
+            if (experience == 3) {
+                System.out.println("Bonus Approved"); 
+            } else {
+                System.out.println("More experience required");
+            }
+        }
+            else {
+                System.out.println("Salary not eligible");
+            }
 
     }
 }
