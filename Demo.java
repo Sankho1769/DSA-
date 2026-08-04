eaae852a44287c59274a615d1abd8c54872437e8
Sankho1769 <banerjeeshubham00@gmail.com>
2026-08-05 00:01:05 +0530

    Day 4 of learning

--- Demo.java
+++ Demo.java
@@ -25,29 +25,51 @@
 //     }
 // }
 
-class Computer {
-   public void playMusic() {
-        System.out.println("Music Playing..");
+// class Computer {
+//    public void playMusic() {
+//         System.out.println("Music Playing..");
+//     }
+
+//     public String getMeAPen(int cost){
+
+//         if (cost >= 10)
+//             return "pen";
+//         else
+//             return "Nothing";
+//     }
+// }
+
+// public class Demo {
+//     public static void main(String[] args) {
+        
+
+//         Computer obj = new Computer();
+//         obj.playMusic();
+//         obj.getMeAPen(10);
+//         String str = obj.getMeAPen(2);
+//         System.out.println(str);
+
+//     }
+// }
+
+class calculator {
+    public int add( int n1, int n2, int n3) {
+        return n1 + n2 + n3;
     }
-
-    public String getMeAPen(int cost){
-
-        if (cost >= 10)
-            return "pen";
-        else
-            return "Nothing";
+    public int add( int n1, int n2) {
+        return n1 + n2;
+    }
+    public double add( double n1, int n2) {
+        return n1 + n2;
     }
 }
 
 public class Demo {
     public static void main(String[] args) {
         
-
-        Computer obj = new Computer();
-        obj.playMusic();
-        obj.getMeAPen(10);
-        String str = obj.getMeAPen(2);
-        System.out.println(str);
-
+        calculator obj = new calculator();
+        int r1 = obj.add(3,4);
+         r1 = obj.add(3,4);
+        System.out.println(r1);
     }
 }
